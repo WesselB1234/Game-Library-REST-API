14b4ca37d62f4baddd49595e1da904dbe4eb13c1
WesselB1234 <stosin007@hotmail.com>
2026-04-16 13:22:16 +0200

    Hello world

---
+++ GameLibrary/src/main/java/GameLibrary/controllers/HelloWorldController.java
@@ -0,0 +1,13 @@
+package GameLibrary.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/")
+    public String sayHi() {
+        return "hi";
+    }
+}
