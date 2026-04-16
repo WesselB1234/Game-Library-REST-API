e14bcff9e66b04027f1b63a6e04d51522fa763ae
WesselB1234 <stosin007@hotmail.com>
2026-04-16 17:25:50 +0200

    Add request body params and path params

--- GameLibrary/src/main/java/GameLibrary/controllers/GameController.java
+++ GameLibrary/src/main/java/GameLibrary/controllers/GameController.java
@@ -2,10 +2,14 @@ package GameLibrary.controllers;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import GameLibrary.models.Game;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 @RestController
 @RequestMapping("/api/games")
@@ -17,22 +21,22 @@ public class GameController {
     }
 
     @GetMapping("/{id}")
-    public String getGameById() {
-        return "get game by id";
+    public String getGameById(@PathVariable Long id) {
+        return "get game by id: " + id;
     }
 
     @PostMapping("/")
-    public String createNewGame() {
-        return "create new game";
+    public String createNewGame(@RequestBody Game game) {
+        return game.getGenre();
     }
 
     @PutMapping("/{id}")
-    public String updateGame() {
-        return "update game";
+    public String updateGame(@RequestBody Game game, @PathVariable Long id) {
+        return "update game " + id + " " + game.getGenre();
     }
 
     @DeleteMapping("/{id}")
-    public String deleteGameById() {
+    public String deleteGameById(@PathVariable Long id) {
         return "delete";
     }
 }
--- GameLibrary/src/main/java/GameLibrary/models/dto/GameDTO.java
+++ GameLibrary/src/main/java/GameLibrary/models/dto/GameDTO.java
@@ -1,5 +1,49 @@
 package GameLibrary.models.dto;
 
 public class GameDTO {
+
+    private Long id; 
+    private String title; 
+    private String genre; 
+    private String platform;
+
+    public GameDTO(Long id, String title, String genre, String platform)
+    {
+        this.id = id;
+        this.title = title;
+        this.genre = genre;
+        this.platform = platform;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public String getPlatform() {
+        return platform;
+    }
     
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public void setPlatform(String platform) {
+        this.platform = platform;
+    }
 }
